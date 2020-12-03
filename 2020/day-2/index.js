fcd81b7c64288e89ee62dcc4e90e00b583e1d0be
Sudhir <rihdus.io@gmail.com>
2020-12-03 14:51:45 +0000

    day 2 - done!

--- 2020/day-2/index.js
+++ 2020/day-2/index.js
@@ -1,1 +1,26 @@
-// Happy coding
+const input = require('./input')
+
+const sample = `
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+`;
+
+const validPasswords = input
+  .split('\n').filter(Boolean)
+  .map((l) => {
+    const match =  l.split(/[: -]/).filter(Boolean)
+    return {
+      min: match[0],
+      max: match[1],
+      char: new RegExp(match[2], 'g'),
+      password: match[3],
+    }
+  })
+  .filter(({min, max, char, password}) => {
+    const numOccurrences = password.length - password.replace(char, '').length;
+    if(numOccurrences < min || numOccurrences > max) return false;
+    return true;
+  })
+
+console.log(validPasswords.length)
